962d26a99f780f7f7c7097be088f9cc5645b8b0f
Kavi <jayathunge.work@gmail.com>
2021-04-08 09:52:06 +0100

    create jekyll site

---
+++ dir_builder.py
@@ -0,0 +1,53 @@
+import os
+import sys
+import glob
+
+OUT_DIR = "./dirs/"
+
+MARKDOWN_TEMPLATE = """
+---
+layout: page
+title: Files
+permalink: /<<>>
+---
+
+# Directory listing for /<<>>
+"""
+
+pwd = sys.argv[1]
+
+def make_listing(text, link, is_dir=False):
+	if is_dir:
+		return f"[**:file_folder: {text}/**](/{link})  \n"
+	else:
+		return f"[**:page_facing_up: {text}**]({text})  \n"
+
+def get_filenames_links(directory):
+	d = [ (p.split('/')[-1], p) for p in  glob.glob(f"{directory}/*")]
+	return d
+
+
+def traverse(directory):
+	d = get_filenames_links(directory)
+	template = MARKDOWN_TEMPLATE.replace("<<>>", directory)
+	dirs = []
+	for name_link in d:
+		name = name_link[0]
+		link = name_link[1]
+		
+		if os.path.isdir(link):
+			dirs.append(link)
+
+		template += make_listing(name, link, os.path.isdir(link))
+
+	filename = f"{OUT_DIR}{directory.replace('/', '-')}.markdown"
+	
+	with open(filename, "w") as fh:
+		fh.write(template)
+
+	for d in dirs:
+		traverse(d)
+# print(MARKDOWN_TEMPLATE.replace("<<>>", pwd))
+
+traverse(pwd)
+
---
+++ index.markdown
@@ -0,0 +1,25 @@
+---
+layout: page
+title: Intel-PA Home
+---
+
+# Welcome to Intel-PA
+
+  This is a landing page that is meant to provide an overview of the project and where to find various resources that pertain to it.
+
+### Modules and Ownership
+
+- Speech recognition module - Jiajun Huang
+- Natural language understanding system - Xiaoxiao Liu, Ruibin Wang
+- Medical diagnosis module - Ruibin Wang
+- Natural language generation module - Xiaoxiao Liu
+- Speech generation module - Kavi Jayathunge
+
+### Static files
+  Files generated over the course of the work - graphs, images, reports etc can be found [here](/static/)
+
+### Literature
+  This is a repository of academic writing that has been read so far, along with comments about its relevance to the project. (Maybe link to a Mendeley library here?)
+  
+### Datasets
+  A collection of relevant datasets with appropriate statistics (size, is it noisy etc). Link to a google drive.
