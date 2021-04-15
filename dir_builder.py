2bdb7069d5abe20e56145ebd52e6e17e8e4f34e8
Kavi <jayathunge.work@gmail.com>
2021-04-15 15:40:55 +0100

    fix typo

--- dir_builder.py
+++ dir_builder.py
@@ -4,32 +4,70 @@ import glob
 
 OUT_DIR = "./dirs/"
 
-MARKDOWN_TEMPLATE = """---
+MD_TEMPLATE = """---
 layout: page
 title: Files
-permalink: /<<>>/
+permalink: /{permalink}/
 ---
 
-# Directory listing for /<<>>
-[**<-back**](/>><<)  
+# Directory listing for /{permalink}
+[**<-back**](/{parent})  
+"""
+
+
+USER_MD_TEMPLATE = """---
+layout: page
+title: {title}
+permalink: /{permalink}/{title}/
+---
+
+[**<-back**](/{permalink})  
 """
 
 pwd = sys.argv[1]
+
+def clean():
+	files = glob.glob(f'{OUT_DIR}*')
+	for f in files:
+	    os.remove(f)
+
+
 
 def make_listing(text, link, is_dir=False):
 	if is_dir:
 		return f"[**:file_folder: {text}/**](/{link})  \n"
 	else:
-		return f"[**:page_facing_up: {text}**]({text})  \n"
+		if text.endswith(".md") or text.endswith(".markdown"):
+			filename = text.split('.')[0]
+			return f"[**:page_facing_up: {text}**]({filename}) ([download]({text}))  \n"
+		else:
+			return f"[**:page_facing_up: {text}**]({text})  \n"
 
 def get_filenames_links(directory):
 	d = [ (p.split('/')[-1], p) for p in  glob.glob(f"{directory}/*")]
 	return d
 
+def markdown_to_jekyll_page(filepath):
+	filepath_l = filepath.split('/')
+	filename = filepath_l[-1]
+	t = filename.split('.')[0]
+	p = '/'.join(filepath_l[:-1])
+
+	content = USER_MD_TEMPLATE.format(title=t, permalink=p) 
+	with open(filepath, "r") as fh:
+		line = fh.readline()
+		content += f"{line}"
+		while line:
+			line = fh.readline()
+			content += f"{line}"
+
+
+	return content, t
+
 
 def traverse(parent, directory):
 	d = get_filenames_links(directory)
-	template = MARKDOWN_TEMPLATE.replace("<<>>", directory).replace(">><<", parent)
+	template = MD_TEMPLATE.format(permalink=directory, parent=parent)
 	dirs = []
 	for name_link in d:
 		name = name_link[0]
@@ -37,6 +75,11 @@ def traverse(parent, directory):
 		
 		if os.path.isdir(link):
 			dirs.append(link)
+		elif link.endswith(".md") or link.endswith(".markdown"):
+			content, fname = markdown_to_jekyll_page(link)
+
+			with open(f"{OUT_DIR}{fname}.markdown", "w") as fh:
+				fh.write(content)
 
 		template += make_listing(name, link, os.path.isdir(link))
 
@@ -49,5 +92,10 @@ def traverse(parent, directory):
 		traverse(directory, d)
 # print(MARKDOWN_TEMPLATE.replace("<<>>", pwd))
 
+
+
+clean()
 traverse("", pwd)
+
+# print(markdown_to_jekyll_page("./static/kavi/progress_reports/kavi_31_03_2021_progress_report.md"))
 
